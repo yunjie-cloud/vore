cad04807de5c377c37bb4276eb4d6d39a5b86a9a
Yunjie Shin <yujieshin7@email.com>
2026-04-07 18:21:49 +0800

    push google

--- Project/login.php
+++ Project/login.php
@@ -12,6 +12,13 @@
       <input type="email" name="email" placeholder="Email" required><br>
       <input type="password" name="password" placeholder="Password" required><br>
       <button type="submit" name="login">Login</button>
+      <div class="google-login">
+  <a href="google_login.php" class="google-btn">
+    <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google logo">
+    Continue with Google
+  </a>
+</div>
+
     </form>
     <a href="index.html" class="back-btn">← Back to Home</a>
   </div>
